3b608b35f2162a043e6cd9d4b5b18ff7dad3f659
Tudor-Florin Ghita <tudor.ghita@belden.com>
2023-05-24 11:30:52 +0300

    Separate by folders

--- build.py
+++ build.py
@@ -1,3 +1,8 @@
-file = open("output.bin", "w")
+import sys
+
+target_dir = sys.argv[1]
+fw_name = sys.argv[2]
+
+file = open(target_dir + "/" + fw_name, "w")
 file.write("This is the firmware\n")
 file.close()
